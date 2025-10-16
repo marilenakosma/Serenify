09a2618a7b431aab1f032c0619963bfcc7495e1e
Marilena Kosma <150340273+marilenakosma@users.noreply.github.com>
2025-10-16 16:08:17 +0300

    Added fonts

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,31 @@
-import { Image, StyleSheet } from 'react-native';
+import { ActivityIndicator, Image, StyleSheet, Text, View } from 'react-native';
 import LogoGreen from "../assets/images/leaf-green.png";
 //import LogoBeige from "../assets/images/leaf-beige.png";
+import { Montserrat_600SemiBold } from "@expo-google-fonts/montserrat";
+import { useFonts } from 'expo-font';
 import ThemedText from "../components/ThemedText";
 import ThemedView from "../components/ThemedView";
 
 export default function Index() {
+  const [fontsLoaded] = useFonts({
+    Montserrat_600SemiBold
+  });
+
+  if (!fontsLoaded) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size="large" color="#0000ff" />
+        <Text>Loading fonts...</Text>
+      </View>
+    );
+  }
+
   return (
     <ThemedView style={styles.container}>
       
       <Image source={LogoGreen} style={styles.image}/>
-      <ThemedText title={true} style={styles.title}>
+
+      <ThemedText title={true} style={styles.title_alt}>
        Serenify
       </ThemedText>
 
@@ -23,14 +39,14 @@ const styles = StyleSheet.create({
         alignItems:'center',
         justifyContent:'center'
     },
-    title: {
-        fontWeight: 'bold',
-        fontSize: 18
+     title_alt: {
+        fontSize: 20,
+        fontFamily: 'Montserrat_600SemiBold'
     },
     image: {
         marginVertical: 20,
-        width:200,
-        height:200
+        width:220,
+        height:220
     },
     link: {
         marginVertical: 10,
